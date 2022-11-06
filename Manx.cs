ea40505d13dec2a3c1bc1edc3c40e1044865e5d4
Alfred <alfred.co95@hotmail.com>
2022-11-06 18:58:22 +0100

    Made some changes to the Main method

--- Manx.cs
+++ Manx.cs
@@ -7,9 +7,9 @@ namespace Animals_Alfred_SUT22
     public class Manx : Cat
     {
         public bool tail;
-        public Manx(int _claws, int _legs, string _food, int _animalId, float _weight, float _speed, bool tail) : base(_claws, _legs, _food, _animalId, _weight, _speed)
+        public Manx(int _claws, int _legs, string _food, int _animalId, float _weight, float _speed, bool _tail) : base(_claws, _legs, _food, _animalId, _weight, _speed)
         {
-            tail = false;
+            _tail = tail;
         }
     }
 }
--- Sphynx.cs
+++ Sphynx.cs
@@ -7,9 +7,9 @@ namespace Animals_Alfred_SUT22
     public class Sphynx : Cat
     {
         bool fur;
-        public Sphynx(int _claws, int _legs, string _food, int _animalId, float _weight, float _speed, bool Fur) : base(_claws, _legs, _food, _animalId, _weight, _speed)
+        public Sphynx(int _claws, int _legs, string _food, int _animalId, float _weight, float _speed, bool _fur) : base(_claws, _legs, _food, _animalId, _weight, _speed)
         {
-            fur = Fur;
+            fur = _fur;
         }
     }
 }
